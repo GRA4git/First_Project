b7f6bd91829b99a2da42ade6a9f6b842f708ed9b
Gra4git <Gra4git@gmail.com>
2021-11-04 15:58:12 +0300

    Ruslan

--- server/controllers/news.controller.js
+++ server/controllers/news.controller.js
@@ -1,1 +1,44 @@
-123
+const e = require("express")
+const News = require("../models/News.model")
+
+module.exports.newsController = {
+    addNews: async((req,res)=>{
+        try {
+            News.create({
+                heading: req.body.heading,
+                text: req.body.text,
+                category: req.body.category,
+                img: req.body.img
+            })
+            res.json('news add')
+        } catch (error) {
+            res.json("news add error:" + error )
+        }
+    }),
+    getNews: async((req,res)=>{
+        try {
+           const news = await News.find()
+           return res.json(news) 
+        } catch (error) {
+            res.json( "ne polu4ilos" + error)
+        }
+    }),
+    getNewsByCategory: async((req,res) =>{
+        try {
+            const newsCategory = await News.find({category: req.params.idCategory})
+            return res.json(newsCategory)
+        } catch (error) {
+            return res.json(error)
+        }
+    }),
+    getNewsById: async (req, res) => {
+        try {
+            const news = await News.findById(req.params.idNews)
+            res.json(news)
+        } catch (error) {
+            return res.json(error)
+        }
+    }
+}
+
+
--- server/routes/news.router.js
+++ server/routes/news.router.js
@@ -1,1 +1,9 @@
-123
+const { Router } = require("express")
+const {newsController} = require('../controllers/news.controller')
+
+const router = Router()
+
+router.post( "/add", newsController.addNews)
+router.get( "/", newsController.getNews)
+router.get( "/category/:idCategory", newsController.addNews)
+router.get( "/:idNews", newsController.addNews)
